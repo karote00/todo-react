a010e78d3b74790a9d705559a741f65a07f94bd9
ASA <karoteglathe@gmail.com>
2016-05-01 01:35:04 +0800

    use react-router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,91 @@
-import React from 'react';
+require('../stylesheets/app.scss');
+import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { App } from './App';
+import { Router, Route, Link, hashHistory } from 'react-router';
+import { TabContent } from './TabContent';
+import { createStore } from 'redux';
+import { Tabs } from './Tabs';
 
-render(<App />, document.getElementById('root'));
+function tabActive(state, action) {
+  if (state == undefined) return 'Starred';
+  switch (action.type) {
+	case 'All':
+	  return 'All';
+	case 'Starred':
+	  return 'Starred';
+	case 'Active':
+	  return 'Active';
+	case 'Complete':
+	  return 'Complete';
+	default:
+	  return 'All';
+  }
+}
+
+var tabsStore = createStore(tabActive);
+
+class AllContent extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		tabsStore.dispatch({type: 'All'});
+		return (
+			<TabContent />
+		);
+	}
+}
+
+class StarredContent extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		tabsStore.dispatch({type: 'Complete'});
+		return (
+			<TabContent />
+		);
+	}
+}
+
+class ActiveContent extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		tabsStore.dispatch({type: 'Active'});
+		return (
+			<TabContent />
+		);
+	}
+}
+
+class CompleteContent extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		tabsStore.dispatch({type: 'Complete'});
+		return (
+			<TabContent />
+		);
+	}
+}
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={Tabs}>
+      <Route>
+        <Route path="All" component={AllContent} />
+        <Route path="Starred" component={StarredContent} />
+        <Route path="Active" component={ActiveContent} />
+        <Route path="Complete" component={CompleteContent} />
+      </Route>
+    </Route>
+  </Router>
+  ), document.getElementById('root')
+);
