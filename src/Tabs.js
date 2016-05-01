27c39d53e6aea3c4ca00887ad3fd6d28a3b6113a
ASA <karoteglathe@gmail.com>
2016-05-01 14:49:15 +0800

    update Tabs to use react-router

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,33 +1,42 @@
 require('../stylesheets/tabs.scss');
 import React, { Component } from 'react';
-import { Router, Route, Link } from 'react-router';
-
-
-class TabsList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {'active': 'All'};
-  }
-
-  render() {
-    return (
-      <div className="tabs">
-        <div className={this.state.active == this.props.name? 'active': ''}><Link to='All'>All</Link></div>
-        <div className={this.state.active == this.props.name? 'active': ''}><Link to='Starred'>Starred</Link></div>
-        <div className={this.state.active == this.props.name? 'active': ''}><Link to='Active'>Active</Link></div>
-        <div className={this.state.active == this.props.name? 'active': ''}><Link to='Complete'>Complete</Link></div>
-      </div>
-      <main>
-          {this.props.children}
-      </main>
-    )
-  }
-}
+import { Router, Route, Link, hashHistory } from 'react-router';
+import { tabsStore } from './tabState';
 
 export class Tabs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {'active': this.props.location.pathname.substr(1)};
+    // console.log(this)
+    var currentLocation = this.props.location.pathname.substr(1);
+    console.log(currentLocation)
+    // console.log(hashHistory)
+    // console.log(Router)
+    // console.log(Route)
+    // var currentRouteName = Router.getCurrentPathname();
+    // console.log(currentRouteName)
+  // console.log(Route.transitionTo(currentRouteName, {lang: 'de'}));
+
+  }
+
+  handleChangeLang(e) {
+    console.log(e)
+  }
+
   render() {
+    console.log(tabsStore.getState())
     return (
-      <TabsList />
+      <div>
+        <div className="tabs">
+          <div className={this.state.active == 'All'? 'active': ''}><Link to='All'>All</Link></div>
+          <div className={this.state.active == 'Starred'? 'active': ''}><Link to='Starred'>Starred</Link></div>
+          <div className={this.state.active == 'Active'? 'active': ''}><Link to='Active'>Active</Link></div>
+          <div className={this.state.active == 'Complete'? 'active': ''}><Link to='Complete'>Complete</Link></div>
+        </div>
+        <div>
+            {this.props.children}
+        </div>
+      </div>
     )
   }
 }
