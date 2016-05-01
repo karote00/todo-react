d4f66f136611358bc3ca0e3d30b693fa4ad3f622
ASA <karoteglathe@gmail.com>
2016-05-01 14:49:49 +0800

    add simple TabContent for every tab

---
+++ src/TabContent.js
@@ -0,0 +1,22 @@
+require('../stylesheets/app.scss');
+import React, { Component } from 'react';
+
+class Content extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Content</div>
+    )
+  }
+}
+
+export class TabContent extends Component {
+  render() {
+    return (
+      <div>Content</div>
+    )
+  }
+}
