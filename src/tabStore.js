5dd94667a389c7f380194cadf0d5b7c8425f1b38
ASA <karoteglathe@gmail.com>
2016-05-02 23:07:24 +0800

    update tabStore for Get and Add

--- src/tabStore.js
+++ src/tabStore.js
@@ -1,27 +1,41 @@
 import { createStore } from 'redux';
 
 var allList = [],
-	starredList = [],
-	activeList = [],
-	completeList = [];
-
+    starredList = [],
+    activeList = [],
+    completeList = [];
 
 function tabActive(state = 0, action) {
-  var list = [];
-  switch (action.type) {
-	case 'All':
-	  list = allList;
-	case 'Starred':
-	  list = starredList;
-	case 'Active':
-	  list = activeList;
-	case 'Complete':
-	  list = completeList;
-	default:
-	  list = allList;
-  }
+    var list = [];
 
-  return list;
+    switch (action.type) {
+        case 'Get':
+            list = getList(action);
+            break;
+        case 'Add':
+            allList.push(action.item);
+            list = getList(action);
+            break;
+    }
+
+    return list;
+}
+
+function getList(action) {
+	var list = [];
+	switch (action.path) {
+        case 'All':
+            list = allList;
+        case 'Starred':
+            list = starredList;
+        case 'Active':
+            list = activeList;
+        case 'Complete':
+            list = completeList;
+        default:
+            list = allList;
+    }
+    return list;
 }
 
 var tabStore = createStore(tabActive);
