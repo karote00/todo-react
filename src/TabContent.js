d3440a165590dae10b9523427570aac3cb0af2fd
ASA <karoteglathe@gmail.com>
2016-05-02 21:05:59 +0800

    use item and tabStore

--- src/TabContent.js
+++ src/TabContent.js
@@ -1,25 +1,41 @@
 require('../stylesheets/content.scss');
 import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+import { tabStore } from './tabStore';
+import { Item } from './Item';
 
 export class TabContent extends Component {
   constructor(props) {
     super(props);
+    var path = this.props.location.pathname && this.props.location.pathname.substr(1) || 'All';
+    tabStore.dispatch({type: path});
+    this.state = {
+      list: tabStore.getState(),
+      todo: ''
+    };
   }
 
-  handleChange(str) {
-    console.log(str)
+  handleChange(e) {
+    this.setState({todo: e.target.value});
+  }
+
+  addList() {
+    if (this.state.todo) this.state.list.push(this.state.todo);
+    this.setState({todo: ''});
   }
 
   render() {
-    var filterLink = {
-      requestChange: this.handleChange
-    };
     return (
       <div>
-        <div className="search">
-          <input type="text" valueLink={filterLink} placeholder="Type something..." />
+        <div className="newTodo">
+          <input type="text" value={this.state.todo} onChange={this.handleChange.bind(this)} placeholder="Type something..." />
+          <FontAwesome name="plus" className="todo-plus" size="2x" onClick={this.addList.bind(this)} />
         </div>
-        <div>Content</div>
+        <div>
+          {this.state.list.map(function(item, i) {
+            return <Item key={i} data={item}/>;
+          })}
+        </div>
       </div>
     )
   }
